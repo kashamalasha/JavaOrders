f601ca33cefe22cd3d12e9dc3a71ef5b9b87f610
kashamalasha <dmitry.burnyshev@gmail.com>
2015-11-06 23:27:27 +0300

    добавил проект из IDEA

---
+++ src/mvc/Controller.java
@@ -0,0 +1,59 @@
+package mvc;
+
+/**
+ * Абстрактный класс контроллера
+ */
+public class Controller {
+
+    //Атрибуты класса
+    private final Model model;    /** Модель данных */
+    private final View view;      /** Представление данных */
+
+    //Конструктор
+    public Controller(Model model, View view){
+        this.model = model;
+        this.view = view;
+    }
+
+    //Методы класса
+
+    /**
+     * Задать имя
+     * @param name Имя
+     */
+    public void setName(String name){
+        model.setName(name);
+    }
+
+    /**
+     * Получить Имя
+     * @return Имя
+     */
+    public String getName(){
+        return model.getName();
+    }
+
+    /**
+     * Задать ID
+     * @param ID
+     */
+    public void setID(int ID){
+        model.setID(ID);
+    }
+
+    /**
+     * Получить ID
+      * @return
+     */
+    public int getID(){
+        return model.getID();
+    }
+
+    /**
+     * Обновить представление
+     */
+    public void updateView(){
+        view.show(model.getName(), model.getID());
+    }
+
+}
---
+++ src/mvc/View.java
@@ -0,0 +1,118 @@
+package mvc;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+
+/**
+ * Представление данных
+ */
+public class View {
+
+    public void show(String name, int ID){
+        System.out.println(ID + ": " + name);
+    }
+
+    public Scanner sc = new Scanner(System.in);
+
+    public void clearScreen() {
+        //Очистка экрана - строка ввода в нижней части экрана
+        /*for (int i = 0; i < 100; i++) {
+            System.out.println("\b");
+        }*/
+        //Очистка экрана - строка ввода в верхней части экрана
+        final String OS = System.getProperty("os.name");
+        if (OS.contains("Windows")) {
+            try {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            final String ANSI_CLS = "\u001b[2J";
+            final String ANSI_HOME = "\u001b[H";
+            System.out.print(ANSI_HOME + ANSI_CLS);
+            System.out.flush();
+        }
+    }
+
+    public int getMenuItem() {
+        int select = 0;
+        this.showMainMenu();
+        select = sc.nextInt();
+        sc.nextLine();
+        select = this.getSubMenu(select);
+        return select;
+    }
+
+    public void showMainMenu(){
+        this.clearScreen();
+        System.out.println();
+        System.out.println("  <=  Меню   =>  ");
+        System.out.println("  -------------  ");
+        System.out.println("  1 = Клиенты    ");
+        System.out.println("  2 = Заказы     ");
+        System.out.println("  3 = Выход      ");
+        System.out.println("  -------------  ");
+        System.out.print("  Command => ");
+    }
+
+    public int getSubMenu(int select){
+        select *= 10;
+        switch (select){
+            case 10:
+                this.showClientMenu();
+                break;
+            case 20:
+                this.showOrderMenu();
+                break;
+            case 30:
+                this.showExitMenu();
+                break;
+            default:
+                this.getSubMenu(select);
+                break;
+        }
+        select += sc.nextInt();
+        return select;
+    }
+
+    public void showExitMenu(){
+        this.clearScreen();
+        System.out.println();
+        System.out.println("  <=  Выход  =>  ");
+        System.out.println("  -------------  ");
+        System.out.println("  1 = Назад      ");
+        System.out.println("  2 = Выход      ");
+        System.out.println();
+        System.out.println("  -------------  ");
+        System.out.print("  Command => ");
+    }
+
+    public void showClientMenu(){
+        this.clearScreen();
+        System.out.println();
+        System.out.println("  <= Клиенты =>  ");
+        System.out.println("  -------------  ");
+        System.out.println("  1 = Назад      ");
+        System.out.println("  2 = Просмотр   ");
+        System.out.println();
+        System.out.println("  -------------  ");
+        System.out.print("  Command => ");
+    }
+
+    public void showOrderMenu(){
+        this.clearScreen();
+        System.out.println();
+        System.out.println("  <= Заказы  =>  ");
+        System.out.println("  -------------  ");
+        System.out.println("  1 = Назад      ");
+        System.out.println("  2 = Просмотр   ");
+        System.out.println();
+        System.out.println("  -------------  ");
+        System.out.print("  Command => ");
+    }
+
+}
